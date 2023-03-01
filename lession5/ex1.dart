9cd09a51c116051899fe94b42e401ab9f371837c
Phạm Thị Quỳnh Anh <115700329+anh0701@users.noreply.github.com>
2023-03-01 16:23:41 +0700

    update

--- lession5/ex1.dart
+++ lession5/ex1.dart
@@ -2,13 +2,7 @@ import 'dart:io';
 
 void main(List<String> args) {
   File file = File("hello.txt");
-  file.writeAsStringSync('Name\n');
-  for (int i = 0; i < 3; i++) {
-    
-    stdout.write("Enter your name ${i + 1}: ");
-    String? name = stdin.readLineSync();
-    file.writeAsStringSync('$name\n', mode: FileMode.append);
-  }
+  file.writeAsStringSync('Pham Thi Quynh Anh\n');
   String contents = file.readAsStringSync();
   print(contents);
 }
